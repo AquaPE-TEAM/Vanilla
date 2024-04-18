76c0b4efa2baf5c225033abdebc3ee72acfb5e19
OneWearf4k <123255601+OneWearf4k@users.noreply.github.com>
2024-04-18 23:49:06 +0300

    todo

---
+++ src/main/java/ru/onewearf/aquape/module/teleport/Teleport.java
@@ -0,0 +1,15 @@
+package ru.onewearf.aquape.module.teleport;
+
+import ru.onewearf.aquape.module.Module;
+
+public class Teleport extends Module {
+    @Override
+    public String getName() {
+        return "Module";
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
